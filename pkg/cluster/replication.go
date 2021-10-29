0fd0b82a0ba49d5238d2824818f92a7f6687b108
Ege Güneş <ege.gunes@percona.com>
2021-10-29 13:21:25 +0300

    K8SPS-18: Configure semi-sync nodes through orchestrator
    Orchestrator 3.2.6 has a new feature to enforce exact number of
    semi-sync nodes. According to `rpl_semi_sync_master_wait_for_slave_count`
    variable it enable/disable semi-sync replication on replica nodes.
    
    If the user configures `sizeSemiSync` in `cr.yaml`, the operator enables semi-sync
    replication on the primary (`set global rpl_semi_sync_master_enabled=1`)
    and set `rpl_semi_sync_master_wait_for_slave_count` to the user defined
    value. Afterwards, Orchestrator will detect a failure due to
    `EnforceExactSemiSyncReplicas` config option of itself and will try to
    configure replicas as semi-sync to reach to the desired state.
    
    If the user sets `sizeSemiSync` to 0 or removes the field, the operator
    disables the semi-sync on primary (`set global rpl_semi_sync_master_enabled=0`).
    Orchestrator won't disable semi-sync on replicas in this scenario but
    it's fine since the replication is async if only one side is semi-sync.
    
    Note that, we're not using the new plugins that are introduced in
    8.0.26. Orchestrator doesn't support this plugins yet and until they're
    supported by it we need to stay on 8.0.25.

--- pkg/cluster/replication.go
+++ pkg/cluster/replication.go
@@ -21,6 +21,10 @@ func (r *MySQLReconciler) reconcileReplication(log logr.Logger, cr *v2.PerconaSe
 		return errors.Wrap(err, "reconcile primary pod")
 	}
 
+	if err := r.reconcileSemiSync(log, cr); err != nil {
+		return errors.Wrap(err, "reconcile semi-sync")
+	}
+
 	return nil
 }
 
@@ -56,6 +60,41 @@ func (r *MySQLReconciler) reconcilePrimaryPod(log logr.Logger, cr *v2.PerconaSer
 		if err := r.Client.Update(context.TODO(), &pod); err != nil {
 			return errors.Wrap(err, "update primary pod")
 		}
+	}
+
+	return nil
+}
+
+func (r *MySQLReconciler) reconcileSemiSync(log logr.Logger, cr *v2.PerconaServerForMySQL) error {
+	o := orchestrator.New(cr)
+	orc := orclient.New(o.APIHost())
+	clusterHint := cr.ClusterHint()
+
+	primary, err := orc.ClusterPrimary(clusterHint)
+	if err != nil {
+		return errors.Wrap(err, "get primary from orchestrator")
+	}
+
+	operatorPass, err := k8s.UserPassword(r.Client, cr, v2.USERS_SECRET_KEY_OPERATOR)
+	if err != nil {
+		return errors.Wrap(err, "get operator password")
+	}
+
+	m, err := mysql.NewReplicator(v2.USERS_SECRET_KEY_OPERATOR, operatorPass, primary.Key.Hostname, int32(33062))
+	if err != nil {
+		return errors.Wrapf(err, "connect to %s", primary.Key.Hostname)
+	}
+
+	if err := m.SetSemiSyncSource(cr.Spec.MySQL.SizeSemiSync > 0); err != nil {
+		return errors.Wrapf(err, "set semi-sync on %s", primary.Key.Hostname)
+	}
+
+	if cr.Spec.MySQL.SizeSemiSync < 1 {
+		return nil
+	}
+
+	if err := m.SetSemiSyncSize(cr.Spec.MySQL.SizeSemiSync); err != nil {
+		return errors.Wrapf(err, "set semi-sync size on %s", primary.Key.Hostname)
 	}
 
 	return nil
