c1ac1dff97ce253386614b51d5fc6f5a6f989389
Ege Güneş <ege.gunes@percona.com>
2022-12-20 14:49:58 +0300

    Apply suggestions from code review
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- build/router_readiness_check.sh
+++ build/router_readiness_check.sh
@@ -2,14 +2,14 @@
 
 OPERATOR_PASS=$(</etc/mysql/mysql-users-secret/operator)
 
-if ! curl -k -s -u operator:${OPERATOR_PASS} https://localhost:8443/api/20190715/routes/bootstrap_rw/health | grep true; then
-    echo "Read-write route is not healthy"
-    exit 1
+if ! curl -k -s -u operator:"${OPERATOR_PASS}" https://localhost:8443/api/20190715/routes/bootstrap_rw/health | grep true; then
+	echo "Read-write route is not healthy"
+	exit 1
 fi
 
-if ! curl -k -s -u operator:${OPERATOR_PASS} https://localhost:8443/api/20190715/routes/bootstrap_ro/health | grep true; then
-    echo "Read-only route is not healthy"
-    exit 1
+if ! curl -k -s -u operator:"${OPERATOR_PASS}" https://localhost:8443/api/20190715/routes/bootstrap_ro/health | grep true; then
+	echo "Read-only route is not healthy"
+	exit 1
 fi
 
 exit 0
