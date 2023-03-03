4d3e17932b9fc992dbcbe619ef93b501d42eacf8
Andrii Dema <a.dema@jazzserve.com>
2023-03-03 12:42:52 +0200

    better way to delete empty secrets

--- pkg/controller/ps/controller.go
+++ pkg/controller/ps/controller.go
@@ -603,12 +603,7 @@ func (r *PerconaServerMySQLReconciler) reconcileUsers(ctx context.Context, cr *a
 
 	log.Info("Discarded old user passwords")
 
-	internalSecret.Data = secret.Data
-	for k, v := range internalSecret.Data {
-		if len(v) == 0 {
-			delete(internalSecret.Data, k)
-		}
-	}
+	internalSecret.Data = secret.DeepCopy().Data
 	if err := r.Client.Update(ctx, internalSecret); err != nil {
 		return errors.Wrapf(err, "update Secret/%s", internalSecret.Name)
 	}
