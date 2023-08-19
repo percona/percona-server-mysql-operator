2a7ec7a5915789c8a4d3b5b72766d67977909ae4
Bilal Khan <heyzabilalyam@gmail.com>
2023-08-19 07:48:36 +0000

    added the comments above the functions in the pkg/haproxy/config.go file

--- pkg/haproxy/config.go
+++ pkg/haproxy/config.go
@@ -17,25 +17,30 @@ const (
 
 type Configurable apiv1alpha1.PerconaServerMySQL
 
+// GetConfigMapName returns ConfigMap name for Configurable.
 func (c *Configurable) GetConfigMapName() string {
 	cr := apiv1alpha1.PerconaServerMySQL(*c)
 	return Name(&cr)
 }
 
+// GetConfigMapKey returns the key for config access.
 func (c *Configurable) GetConfigMapKey() string {
 	return CustomConfigKey
 }
 
+// GetConfiguration returns HAProxy config from PerconaServerMySQL spec.
 func (c *Configurable) GetConfiguration() string {
 	cr := apiv1alpha1.PerconaServerMySQL(*c)
 	return cr.Spec.Proxy.HAProxy.Configuration
 }
 
+// GetResources returns HAProxy's resource requirements from PerconaServerMySQL spec.
 func (c *Configurable) GetResources() corev1.ResourceRequirements {
 	cr := apiv1alpha1.PerconaServerMySQL(*c)
 	return cr.Spec.Proxy.HAProxy.Resources
 }
 
+// ExecuteConfigurationTemplate processes a template using input and memory limit.
 func (c *Configurable) ExecuteConfigurationTemplate(input string, memory *resource.Quantity) (string, error) {
 	tmpl, err := pongo2.FromString(input)
 	if err != nil {
