f38dcc4581769bf36589b873fcf51d8fe1e7cfce
Ege Güneş <ege.gunes@percona.com>
2024-01-24 12:50:54 +0300

    Apply suggestions from code review
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- build/ps-pre-stop.sh
+++ build/ps-pre-stop.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [ ${CLUSTER_TYPE} == "async" ]; then
-  exit 0
+	exit 0
 fi
 
 LOG_FILE=/var/lib/mysql/pre-stop.log
@@ -11,5 +11,5 @@ NAMESPACE=$(</var/run/secrets/kubernetes.io/serviceaccount/namespace)
 OPERATOR_PASSWORD=$(</etc/mysql/mysql-users-secret/operator)
 FQDN="${HOSTNAME}.${SERVICE_NAME}.${NAMESPACE}"
 
-echo "$(date +%Y-%m-%dT%H:%M:%S%Z): Removing ${FQDN} from cluster" >> ${LOG_FILE}
-mysqlsh -i -u operator -p${OPERATOR_PASSWORD} -e "dba.getCluster().removeInstance('${FQDN}:3306')" >> ${LOG_FILE} 2>&1
+echo "$(date +%Y-%m-%dT%H:%M:%S%Z): Removing ${FQDN} from cluster" >>${LOG_FILE}
+mysqlsh -i -u operator -p${OPERATOR_PASSWORD} -e "dba.getCluster().removeInstance('${FQDN}:3306')" >>${LOG_FILE} 2>&1
