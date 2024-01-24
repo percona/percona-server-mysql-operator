fdd21e91adaea862e2541df468991083cf618a73
Ege Güneş <ege.gunes@percona.com>
2024-01-24 11:42:34 +0300

    K8SPS-291: Add prestop hook to MySQL pods

---
+++ build/ps-pre-stop.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+if [ ${CLUSTER_TYPE} == "async" ]; then
+  exit 0
+fi
+
+LOG_FILE=/var/lib/mysql/pre-stop.log
+NAMESPACE=$(</var/run/secrets/kubernetes.io/serviceaccount/namespace)
+OPERATOR_PASSWORD=$(</etc/mysql/mysql-users-secret/operator)
+FQDN="${HOSTNAME}.${SERVICE_NAME}.${NAMESPACE}"
+
+echo "$(date +%Y-%m-%dT%H:%M:%S%Z): Removing ${FQDN} from cluster" >> ${LOG_FILE}
+mysqlsh -i -u operator -p${OPERATOR_PASSWORD} -e "dba.getCluster().removeInstance('${FQDN}:3306')" >> ${LOG_FILE} 2>&1
