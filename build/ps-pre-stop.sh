2b58eff81e00ff7dc20e1183fb44107ccde785b1
Ege Güneş <ege.gunes@percona.com>
2024-01-31 12:38:14 +0300

    fix pre stop hook

--- build/ps-pre-stop.sh
+++ build/ps-pre-stop.sh
@@ -10,6 +10,7 @@ LOG_FILE=/var/lib/mysql/pre-stop.log
 NAMESPACE=$(</var/run/secrets/kubernetes.io/serviceaccount/namespace)
 OPERATOR_PASSWORD=$(</etc/mysql/mysql-users-secret/operator)
 FQDN="${HOSTNAME}.${SERVICE_NAME}.${NAMESPACE}"
+POD_IP=$(hostname -I)
 
 echo "$(date +%Y-%m-%dT%H:%M:%S%Z): Removing ${FQDN} from cluster" >>${LOG_FILE}
-mysqlsh -i -P 33062 -u operator -p"${OPERATOR_PASSWORD}" -e "dba.getCluster().removeInstance('${FQDN}:3306')" >>${LOG_FILE} 2>&1
+mysqlsh -i -h ${POD_IP} -P 33062 -u operator -p"${OPERATOR_PASSWORD}" -e "dba.getCluster().removeInstance('${FQDN}:3306')" >>${LOG_FILE} 2>&1
