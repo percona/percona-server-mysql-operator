9c1444191db65ded667303bf79662ed784713e60
Ege Güneş <ege.gunes@percona.com>
2024-01-31 20:27:39 +0300

    Update build/ps-pre-stop.sh
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- build/ps-pre-stop.sh
+++ build/ps-pre-stop.sh
@@ -13,4 +13,4 @@ FQDN="${HOSTNAME}.${SERVICE_NAME}.${NAMESPACE}"
 POD_IP=$(hostname -I)
 
 echo "$(date +%Y-%m-%dT%H:%M:%S%Z): Removing ${FQDN} from cluster" >>${LOG_FILE}
-mysqlsh -i -h ${POD_IP} -P 33062 -u operator -p"${OPERATOR_PASSWORD}" -e "dba.getCluster().removeInstance('${FQDN}:3306')" >>${LOG_FILE} 2>&1
+mysqlsh -i -h "${POD_IP}" -P 33062 -u operator -p"${OPERATOR_PASSWORD}" -e "dba.getCluster().removeInstance('${FQDN}:3306')" >>${LOG_FILE} 2>&1
