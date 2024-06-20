100f10e72a4840176f2793defe94d84046e52ba7
Inel Pandzic <inel.pandzic@percona.com>
2024-06-20 11:06:54 +0200

    Fix import.

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -35,7 +35,6 @@ import (
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/cache"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	metricsServer "sigs.k8s.io/controller-runtime/pkg/metrics/server"
