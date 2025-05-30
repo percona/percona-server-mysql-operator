313aea6b1cf04078fabb9d6c52275d039f982bc1
Andrii Dema <a.dema@jazzserve.com>
2025-05-30 18:08:15 +0300

    K8SPS-414: add `imagePullSecrets` to the `cr.yaml`
    https://perconadev.atlassian.net/browse/K8SPS-414

---
+++ pkg/router/helpers_test.go
@@ -0,0 +1,43 @@
+package router
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"sigs.k8s.io/yaml"
+
+	apiv1alpha1 "github.com/percona/percona-server-mysql-operator/api/v1alpha1"
+)
+
+func readDefaultCluster(t *testing.T, name, namespace string) *apiv1alpha1.PerconaServerMySQL {
+	t.Helper()
+
+	cr := &apiv1alpha1.PerconaServerMySQL{}
+	readDefaultFile(t, "cr.yaml", cr)
+
+	cr.Name = name
+	cr.Namespace = namespace
+	cr.Spec.InitImage = "init-image"
+	return cr
+}
+
+func readDefaultFile[T any](t *testing.T, filename string, obj *T) {
+	t.Helper()
+
+	data, err := os.ReadFile(filepath.Join("..", "..", "deploy", filename))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if err := yaml.Unmarshal(data, obj); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func updateResource[T any](cr *T, updateFuncs ...func(cr *T)) *T {
+	for _, f := range updateFuncs {
+		f(cr)
+	}
+	return cr
+}
---
+++ pkg/router/router_test.go
@@ -0,0 +1,97 @@
+package router
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/yaml"
+
+	apiv1alpha1 "github.com/percona/percona-server-mysql-operator/api/v1alpha1"
+)
+
+func TestDeployment(t *testing.T) {
+	const ns = "router-ns"
+
+	cr := readDefaultCluster(t, "cluster", ns)
+
+	tests := []struct {
+		name string
+
+		cluster    *apiv1alpha1.PerconaServerMySQL
+		initImage  string
+		configHash string
+		tlsHash    string
+
+		compareFile string
+	}{
+		{
+			name:        "default cr",
+			cluster:     cr.DeepCopy(),
+			initImage:   "init-image",
+			configHash:  "config-hash",
+			tlsHash:     "tls-hash",
+			compareFile: "default-deployment.yaml",
+		},
+		{
+			name: "default cr with image pull secrets",
+			cluster: updateResource(cr.DeepCopy(), func(cr *apiv1alpha1.PerconaServerMySQL) {
+				cr.Spec.Proxy.Router.ImagePullSecrets = []corev1.LocalObjectReference{
+					{
+						Name: "secret-1",
+					},
+					{
+						Name: "secret-2",
+					},
+				}
+			}),
+			initImage:   "init-image",
+			configHash:  "config-hash",
+			tlsHash:     "tls-hash",
+			compareFile: "image-pull-secrets-deployment.yaml",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			d := Deployment(tt.cluster, tt.initImage, tt.configHash, tt.tlsHash)
+			compareObj(t, d, expectedObject[*appsv1.Deployment](t, tt.compareFile))
+		})
+	}
+}
+
+func expectedObject[T client.Object](t *testing.T, path string) T {
+	t.Helper()
+
+	data, err := os.ReadFile(filepath.Join("testdata", path))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	obj := new(T)
+	if err := yaml.Unmarshal(data, obj); err != nil {
+		t.Fatal(err)
+	}
+
+	return *obj
+}
+
+func compareObj[T client.Object](t *testing.T, got, want T) {
+	t.Helper()
+
+	gotBytes, err := yaml.Marshal(got)
+	if err != nil {
+		t.Fatalf("error marshaling got: %v", err)
+	}
+	wantBytes, err := yaml.Marshal(want)
+	if err != nil {
+		t.Fatalf("error marshaling want: %v", err)
+	}
+	if string(gotBytes) != string(wantBytes) {
+		t.Fatal(cmp.Diff(string(wantBytes), string(gotBytes)))
+	}
+}
