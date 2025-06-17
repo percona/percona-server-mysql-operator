242a4f7b291542db7c28e27db5efac8cb4bd7465
Natalia Marukovich <natalia.marukovich@percona.com>
2025-06-17 15:53:20 +0200

    fix PR suggestions and conflict

--- build/router-entrypoint.sh
+++ build/router-entrypoint.sh
@@ -22,7 +22,7 @@ mysqlrouter --force \
 	--conf-set-option http_auth_backend:default_auth_backend.filename="${ROUTER_DIR}/realm.txt" \
 	--directory "${ROUTER_DIR}"
 
-echo ${OPERATOR_PASS_ESCAPED} | mysqlrouter_passwd set "${ROUTER_DIR}/realm.txt" ${OPERATOR_USER}
+echo "${OPERATOR_PASS_ESCAPED}" | mysqlrouter_passwd set "${ROUTER_DIR}/realm.txt" "${OPERATOR_USER}"
 
 sed -i 's/logging_folder=.*/logging_folder=/g' "${ROUTER_DIR}/mysqlrouter.conf"
 sed -i "/\[logger\]/a destination=/dev/stdout" "${ROUTER_DIR}/mysqlrouter.conf"
