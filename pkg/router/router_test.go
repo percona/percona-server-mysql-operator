14602f1fc96454318fba5e7e5bc522155d4dda29
Andrii Dema <a.dema@jazzserve.com>
2025-06-20 16:24:30 +0300

    update unit-test

--- pkg/router/router_test.go
+++ pkg/router/router_test.go
@@ -1,8 +1,7 @@
 package router
 
 import (
-	"os"
-	"path/filepath"
+	"reflect"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -120,14 +119,54 @@ func TestDeployment(t *testing.T) {
 }
 
 func TestPorts(t *testing.T) {
+	defaultPorts := func() []corev1.ServicePort {
+		return []corev1.ServicePort{
+			{
+				Name: "http",
+				Port: 8443,
+			},
+			{
+				Name: "rw-default",
+				Port: 3306,
+				TargetPort: intstr.IntOrString{
+					IntVal: 6446,
+				},
+			},
+			{
+				Name: "read-write",
+				Port: 6446,
+			},
+			{
+				Name: "read-only",
+				Port: 6447,
+			},
+			{
+				Name: "x-read-write",
+				Port: 6448,
+			},
+			{
+				Name: "x-read-only",
+				Port: 6449,
+			},
+			{
+				Name: "x-default",
+				Port: 33060,
+			},
+			{
+				Name: "rw-admin",
+				Port: 33062,
+			},
+		}
+	}
+
 	tests := []struct {
-		name              string
-		specifiedPorts    []corev1.ServicePort
-		expectedPortsFile string
+		name           string
+		specifiedPorts []corev1.ServicePort
+		expectedPorts  []corev1.ServicePort
 	}{
 		{
-			name:              "default ports",
-			expectedPortsFile: "default-ports.yaml",
+			name:          "default ports",
+			expectedPorts: defaultPorts(),
 		},
 		{
 			name: "additional ports",
@@ -145,7 +184,23 @@ func TestPorts(t *testing.T) {
 					},
 				},
 			},
-			expectedPortsFile: "add-ports.yaml",
+			expectedPorts: updateObject(defaultPorts(), func(ports []corev1.ServicePort) []corev1.ServicePort {
+				ports = append(ports, []corev1.ServicePort{
+					{
+						Name: "additional port",
+						Port: 4308,
+					},
+					{
+						Name: "additional port with target port",
+						Port: 1337,
+						TargetPort: intstr.IntOrString{
+							Type:   intstr.Int,
+							IntVal: 20,
+						},
+					},
+				}...)
+				return ports
+			}),
 		},
 		{
 			name: "modified ports with additional ports",
@@ -175,7 +230,37 @@ func TestPorts(t *testing.T) {
 					},
 				},
 			},
-			expectedPortsFile: "mod-add-ports.yaml",
+			expectedPorts: updateObject(defaultPorts(), func(ports []corev1.ServicePort) []corev1.ServicePort {
+				for i, v := range ports {
+					if v.Name == "http" {
+						ports[i].Port = 5555
+						continue
+					}
+					if v.Name == "rw-default" {
+						ports[i].Port = 6666
+						ports[i].TargetPort = intstr.IntOrString{
+							Type:   intstr.Int,
+							IntVal: 30,
+						}
+						continue
+					}
+				}
+				ports = append(ports, []corev1.ServicePort{
+					{
+						Name: "additional port",
+						Port: 4308,
+					},
+					{
+						Name: "additional port with target port",
+						Port: 1337,
+						TargetPort: intstr.IntOrString{
+							Type:   intstr.Int,
+							IntVal: 20,
+						},
+					},
+				}...)
+				return ports
+			}),
 		},
 		{
 			name: "modified port with default targetPort",
@@ -185,55 +270,39 @@ func TestPorts(t *testing.T) {
 					Port: 6666,
 				},
 			},
-			expectedPortsFile: "mod-def-targetport-ports.yaml",
+			expectedPorts: updateObject(defaultPorts(), func(ports []corev1.ServicePort) []corev1.ServicePort {
+				for i, v := range ports {
+					if v.Name == "rw-default" {
+						ports[i].Port = 6666
+						break
+					}
+				}
+				return ports
+			}),
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got := ports(tt.specifiedPorts)
-			data, err := yaml.Marshal(got)
-			if err != nil {
-				t.Fatal(err)
+			if !reflect.DeepEqual(got, tt.expectedPorts) {
+				gotBytes, err := yaml.Marshal(got)
+				if err != nil {
+					t.Fatalf("error marshaling got: %v", err)
+				}
+				wantBytes, err := yaml.Marshal(tt.expectedPorts)
+				if err != nil {
+					t.Fatalf("error marshaling want: %v", err)
+				}
+				t.Fatal(cmp.Diff(string(wantBytes), string(gotBytes)))
 			}
-			err = os.WriteFile(filepath.Join("testdata", "ports", tt.expectedPortsFile), data, 0666)
-			if err != nil {
-				t.Fatal(err)
-			}
-			expected := expectedObject[[]corev1.ServicePort](t, filepath.Join("ports", tt.expectedPortsFile))
-			compareObj(t, got, expected)
 		})
 	}
 }
 
-func expectedObject[T any](t *testing.T, path string) T {
-	t.Helper()
-
-	data, err := os.ReadFile(filepath.Join("testdata", path))
-	if err != nil {
-		t.Fatal(err)
+func updateObject[T any](obj T, updateFuncs ...func(obj T) T) T {
+	for _, f := range updateFuncs {
+		obj = f(obj)
 	}
-
-	obj := new(T)
-	if err := yaml.Unmarshal(data, obj); err != nil {
-		t.Fatal(err)
-	}
-
-	return *obj
-}
-
-func compareObj[T any](t *testing.T, got, want T) {
-	t.Helper()
-
-	gotBytes, err := yaml.Marshal(got)
-	if err != nil {
-		t.Fatalf("error marshaling got: %v", err)
-	}
-	wantBytes, err := yaml.Marshal(want)
-	if err != nil {
-		t.Fatalf("error marshaling want: %v", err)
-	}
-	if string(gotBytes) != string(wantBytes) {
-		t.Fatal(cmp.Diff(string(wantBytes), string(gotBytes)))
-	}
+	return obj
 }
