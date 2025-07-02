1d949ed06def912fb6147bb52b02253ed1dd7156
Andrii Dema <a.dema@jazzserve.com>
2025-07-02 20:38:05 +0300

    K8SPS-414: add `imagePullSecrets` to the `cr.yaml` (#921)
    * K8SPS-414: add `imagePullSecrets` to the `cr.yaml`
    
    https://perconadev.atlassian.net/browse/K8SPS-414
    
    * K8SPS-415: add `runtimeClassName` to the `cr.yaml`
    
    https://perconadev.atlassian.net/browse/K8SPS-415
    
    * K8SPS-416: add tolerations to the `cr.yaml`
    
    https://perconadev.atlassian.net/browse/K8SPS-416
    
    * fix manifests
    
    * small fixes
    
    * update mysql, router, xtrabackup tests
    
    * delete haproxy and orchestrator unit-tests before merge
    
    * update unit-test
    
    * fix lint
    
    ---------
    
    Co-authored-by: Viacheslav Sarzhan <slava.sarzhan@percona.com>

--- pkg/orchestrator/orchestrator_test.go
+++ pkg/orchestrator/orchestrator_test.go
@@ -4,50 +4,122 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/utils/ptr"
 
-	apiv1alpha1 "github.com/percona/percona-server-mysql-operator/api/v1alpha1"
-	"github.com/percona/percona-server-mysql-operator/pkg/naming"
+	"github.com/percona/percona-server-mysql-operator/pkg/platform"
+	"github.com/percona/percona-server-mysql-operator/pkg/version"
 )
 
 func TestStatefulSet(t *testing.T) {
-	tlsHash := "123abc"
-	initImage := "percona/init:latest"
+	const ns = "orc-ns"
+	const initImage = "init-image"
+	const tlsHash = "tls-hash"
 
-	cr := &apiv1alpha1.PerconaServerMySQL{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "cluster1",
-			Namespace: "test-ns",
-		},
-		Spec: apiv1alpha1.PerconaServerMySQLSpec{
-			Orchestrator: apiv1alpha1.OrchestratorSpec{
-				Enabled: true,
-				PodSpec: apiv1alpha1.PodSpec{
-					Size:                          3,
-					TerminationGracePeriodSeconds: pointerInt64(30),
-				},
-			},
-		},
+	cr := readDefaultCluster(t, "cluster", ns)
+	if err := cr.CheckNSetDefaults(t.Context(), &platform.ServerVersion{
+		Platform: platform.PlatformKubernetes,
+	}); err != nil {
+		t.Fatal(err)
 	}
 
-	sts := StatefulSet(cr, initImage, tlsHash)
+	t.Run("object meta", func(t *testing.T) {
+		cluster := cr.DeepCopy()
 
-	assert.NotNil(t, sts)
-	assert.Equal(t, "cluster1-orc", sts.Name)
-	assert.Equal(t, "test-ns", sts.Namespace)
-	assert.Equal(t, int32(3), *sts.Spec.Replicas)
+		sts := StatefulSet(cluster, initImage, tlsHash)
 
-	val, ok := sts.Spec.Template.Annotations[string(naming.AnnotationTLSHash)]
-	assert.True(t, ok)
-	assert.Equal(t, tlsHash, val)
+		assert.NotNil(t, sts)
+		assert.Equal(t, "cluster-orc", sts.Name)
+		assert.Equal(t, "orc-ns", sts.Namespace)
+		labels := map[string]string{
+			"app.kubernetes.io/name":       "orchestrator",
+			"app.kubernetes.io/part-of":    "percona-server",
+			"app.kubernetes.io/version":    "v" + version.Version(),
+			"app.kubernetes.io/instance":   "cluster",
+			"app.kubernetes.io/managed-by": "percona-server-mysql-operator",
+			"app.kubernetes.io/component":  "orchestrator",
+		}
+		assert.Equal(t, labels, sts.Labels)
+	})
 
-	initContainers := sts.Spec.Template.Spec.InitContainers
-	assert.Len(t, initContainers, 1)
-	assert.Equal(t, initImage, initContainers[0].Image)
+	t.Run("defaults", func(t *testing.T) {
+		cluster := cr.DeepCopy()
 
-	assert.Equal(t, pointerInt64(30), sts.Spec.Template.Spec.TerminationGracePeriodSeconds)
-}
+		sts := StatefulSet(cluster, initImage, tlsHash)
 
-func pointerInt64(i int64) *int64 {
-	return &i
+		assert.Equal(t, int32(3), *sts.Spec.Replicas)
+		initContainers := sts.Spec.Template.Spec.InitContainers
+		assert.Len(t, initContainers, 1)
+		assert.Equal(t, initImage, initContainers[0].Image)
+
+		assert.Equal(t, map[string]string{
+			"percona.com/last-applied-tls": tlsHash,
+		}, sts.Spec.Template.Annotations)
+	})
+
+	t.Run("termination grace period seconds", func(t *testing.T) {
+		cluster := cr.DeepCopy()
+
+		cluster.Spec.Orchestrator.TerminationGracePeriodSeconds = nil
+		sts := StatefulSet(cluster, initImage, tlsHash)
+		assert.Equal(t, int64(600), *sts.Spec.Template.Spec.TerminationGracePeriodSeconds)
+
+		cluster.Spec.Orchestrator.TerminationGracePeriodSeconds = ptr.To(int64(30))
+
+		sts = StatefulSet(cluster, initImage, tlsHash)
+		assert.Equal(t, int64(30), *sts.Spec.Template.Spec.TerminationGracePeriodSeconds)
+	})
+
+	t.Run("image pull secrets", func(t *testing.T) {
+		cluster := cr.DeepCopy()
+
+		sts := StatefulSet(cluster, initImage, tlsHash)
+		assert.Equal(t, []corev1.LocalObjectReference(nil), sts.Spec.Template.Spec.ImagePullSecrets)
+
+		imagePullSecrets := []corev1.LocalObjectReference{
+			{
+				Name: "secret-1",
+			},
+			{
+				Name: "secret-2",
+			},
+		}
+		cluster.Spec.Orchestrator.ImagePullSecrets = imagePullSecrets
+
+		sts = StatefulSet(cluster, initImage, tlsHash)
+		assert.Equal(t, imagePullSecrets, sts.Spec.Template.Spec.ImagePullSecrets)
+	})
+
+	t.Run("runtime class name", func(t *testing.T) {
+		cluster := cr.DeepCopy()
+		sts := StatefulSet(cluster, initImage, tlsHash)
+		var e *string
+		assert.Equal(t, e, sts.Spec.Template.Spec.RuntimeClassName)
+
+		const runtimeClassName = "runtimeClassName"
+		cluster.Spec.Orchestrator.RuntimeClassName = ptr.To(runtimeClassName)
+
+		sts = StatefulSet(cluster, initImage, tlsHash)
+		assert.Equal(t, runtimeClassName, *sts.Spec.Template.Spec.RuntimeClassName)
+	})
+
+	t.Run("tolerations", func(t *testing.T) {
+		cluster := cr.DeepCopy()
+		sts := StatefulSet(cluster, initImage, tlsHash)
+		assert.Equal(t, []corev1.Toleration(nil), sts.Spec.Template.Spec.Tolerations)
+
+		tolerations := []corev1.Toleration{
+			{
+				Key:               "node.alpha.kubernetes.io/unreachable",
+				Operator:          "Exists",
+				Value:             "value",
+				Effect:            "NoExecute",
+				TolerationSeconds: ptr.To(int64(1001)),
+			},
+		}
+		cluster.Spec.Orchestrator.Tolerations = tolerations
+
+		sts = StatefulSet(cluster, initImage, tlsHash)
+		assert.Equal(t, tolerations, sts.Spec.Template.Spec.Tolerations)
+	})
 }
