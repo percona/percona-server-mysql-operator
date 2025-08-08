e557e4e24e8b89ba34db8db3d2c32036aef81156
Natalia Marukovich <natalia.marukovich@percona.com>
2025-08-08 17:15:10 +0200

    K8SPS-405 add liveness and readness probe

--- pkg/pmm/container.go
+++ pkg/pmm/container.go
@@ -1,10 +1,11 @@
 package pmm
 
 import (
-	corev1 "k8s.io/api/core/v1"
-
 	apiv1alpha1 "github.com/percona/percona-server-mysql-operator/api/v1alpha1"
 	"github.com/percona/percona-server-mysql-operator/pkg/k8s"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
+	"reflect"
 )
 
 func Container(
@@ -28,7 +29,7 @@ func Container(
 		})
 	}
 
-	return corev1.Container{
+	container := corev1.Container{
 		Name:            "pmm-client",
 		Image:           pmmSpec.Image,
 		ImagePullPolicy: pmmSpec.ImagePullPolicy,
@@ -36,6 +37,28 @@ func Container(
 		Ports:           ports,
 		Resources:       pmmSpec.Resources,
 		Env:             envs,
+		Lifecycle: &corev1.Lifecycle{
+			PreStop: &corev1.LifecycleHandler{
+				Exec: &corev1.ExecAction{
+					Command: []string{
+						"bash",
+						"-c",
+						"pmm-admin unregister --force",
+					},
+				},
+			},
+		},
+		LivenessProbe: &corev1.Probe{
+			InitialDelaySeconds: 60,
+			TimeoutSeconds:      5,
+			PeriodSeconds:       10,
+			ProbeHandler: corev1.ProbeHandler{
+				HTTPGet: &corev1.HTTPGetAction{
+					Port: intstr.FromInt32(7777),
+					Path: "/local/Status",
+				},
+			},
+		},
 		VolumeMounts: []corev1.VolumeMount{
 			{
 				Name:      apiv1alpha1.BinVolumeName,
@@ -43,6 +66,28 @@ func Container(
 			},
 		},
 	}
+
+	if pmmSpec.LivenessProbes != nil {
+		container.LivenessProbe = pmmSpec.LivenessProbes
+		if reflect.DeepEqual(container.LivenessProbe.ProbeHandler, corev1.ProbeHandler{}) {
+			container.LivenessProbe.ProbeHandler.HTTPGet = &corev1.HTTPGetAction{
+				Port: intstr.FromInt32(7777),
+				Path: "/local/Status",
+			}
+		}
+	}
+
+	if pmmSpec.ReadinessProbes != nil {
+		container.ReadinessProbe = pmmSpec.ReadinessProbes
+		if reflect.DeepEqual(container.ReadinessProbe.ProbeHandler, corev1.ProbeHandler{}) {
+			container.ReadinessProbe.ProbeHandler.HTTPGet = &corev1.HTTPGetAction{
+				Port: intstr.FromInt32(7777),
+				Path: "/local/Status",
+			}
+		}
+	}
+
+	return container
 }
 
 func pmmEnvs(cr *apiv1alpha1.PerconaServerMySQL, secret *corev1.Secret, dbType string) []corev1.EnvVar {
