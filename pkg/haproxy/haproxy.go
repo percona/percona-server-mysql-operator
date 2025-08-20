446729add0f51a0eb3af36545989734098e4d6d0
Andrii Dema <a.dema@jazzserve.com>
2025-08-20 12:13:45 +0300

    add version check

--- pkg/haproxy/haproxy.go
+++ pkg/haproxy/haproxy.go
@@ -291,26 +291,29 @@ func haproxyContainer(cr *apiv1alpha1.PerconaServerMySQL) corev1.Container {
 	}
 	env = append(env, spec.Env...)
 
-	readinessProbe := spec.ReadinessProbe
-	readinessProbe.Exec = &corev1.ExecAction{
-		Command: []string{"/opt/percona/haproxy_readiness_check.sh"},
-	}
-	livenessProbe := spec.LivenessProbe
-	livenessProbe.Exec = &corev1.ExecAction{
-		Command: []string{"/opt/percona/haproxy_liveness_check.sh"},
-	}
+	var readinessProbe, livenessProbe *corev1.Probe
+	if cr.CompareVersion("0.12.0") >= 0 {
+		readinessProbe = &spec.ReadinessProbe
+		readinessProbe.Exec = &corev1.ExecAction{
+			Command: []string{"/opt/percona/haproxy_readiness_check.sh"},
+		}
+		livenessProbe = &spec.LivenessProbe
+		livenessProbe.Exec = &corev1.ExecAction{
+			Command: []string{"/opt/percona/haproxy_liveness_check.sh"},
+		}
 
-	probsEnvs := []corev1.EnvVar{
-		{
-			Name:  "LIVENESS_CHECK_TIMEOUT",
-			Value: fmt.Sprint(livenessProbe.TimeoutSeconds),
-		},
-		{
-			Name:  "READINESS_CHECK_TIMEOUT",
-			Value: fmt.Sprint(readinessProbe.TimeoutSeconds),
-		},
+		probsEnvs := []corev1.EnvVar{
+			{
+				Name:  "LIVENESS_CHECK_TIMEOUT",
+				Value: fmt.Sprint(livenessProbe.TimeoutSeconds),
+			},
+			{
+				Name:  "READINESS_CHECK_TIMEOUT",
+				Value: fmt.Sprint(readinessProbe.TimeoutSeconds),
+			},
+		}
+		env = append(env, probsEnvs...)
 	}
-	env = append(env, probsEnvs...)
 
 	return corev1.Container{
 		Name:            AppName,
@@ -321,8 +324,8 @@ func haproxyContainer(cr *apiv1alpha1.PerconaServerMySQL) corev1.Container {
 		EnvFrom:         spec.EnvFrom,
 		Command:         []string{"/opt/percona/haproxy-entrypoint.sh"},
 		Args:            []string{"haproxy"},
-		ReadinessProbe:  &readinessProbe,
-		LivenessProbe:   &livenessProbe,
+		ReadinessProbe:  readinessProbe,
+		LivenessProbe:   livenessProbe,
 		Ports: []corev1.ContainerPort{
 			{
 				Name:          "mysql",
