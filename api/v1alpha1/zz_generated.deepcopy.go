f97e30873ec9b2341d067478de437a0bb02c7fa9
Natalia Marukovich <natalia.marukovich@percona.com>
2025-08-28 10:49:51 +0200

    fix

--- api/v1alpha1/zz_generated.deepcopy.go
+++ api/v1alpha1/zz_generated.deepcopy.go
@@ -1104,7 +1104,7 @@ func (in *ServiceExpose) DeepCopyInto(out *ServiceExpose) {
 	}
 	if in.InternalTrafficPolicy != nil {
 		in, out := &in.InternalTrafficPolicy, &out.InternalTrafficPolicy
-		*out = new(v1.ServiceInternalTrafficPolicy)
+		*out = new(v1.ServiceInternalTrafficPolicyType)
 		**out = **in
 	}
 }
