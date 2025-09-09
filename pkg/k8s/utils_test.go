d3343b77c75cfa1768b942baf89d9d1653148081
Andrii Dema <a.dema@jazzserve.com>
2025-09-09 11:08:53 +0300

    add unit-test

--- pkg/k8s/utils_test.go
+++ pkg/k8s/utils_test.go
@@ -3,6 +3,7 @@ package k8s
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -14,6 +15,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 
 	apiv1alpha1 "github.com/percona/percona-server-mysql-operator/api/v1alpha1"
+	"github.com/percona/percona-server-mysql-operator/pkg/naming"
 )
 
 func TestEnsureService(t *testing.T) {
@@ -56,7 +58,8 @@ func TestEnsureService(t *testing.T) {
 			validate: func(t *testing.T, cl client.Client, svc *corev1.Service) {
 				result := &corev1.Service{}
 				err := cl.Get(context.Background(), types.NamespacedName{
-					Name: svc.Name, Namespace: svc.Namespace}, result)
+					Name: svc.Name, Namespace: svc.Namespace,
+				}, result)
 				assert.NoError(t, err)
 				assert.Equal(t, "test-service", result.Name)
 				assert.Equal(t, "default", result.Namespace)
@@ -93,7 +96,8 @@ func TestEnsureService(t *testing.T) {
 			validate: func(t *testing.T, cl client.Client, svc *corev1.Service) {
 				result := &corev1.Service{}
 				err := cl.Get(context.Background(), types.NamespacedName{
-					Name: svc.Name, Namespace: svc.Namespace}, result)
+					Name: svc.Name, Namespace: svc.Namespace,
+				}, result)
 				assert.NoError(t, err)
 				assert.Equal(t, "new-service", result.Name)
 				assert.Contains(t, result.GetAnnotations(), "new")
@@ -147,7 +151,8 @@ func TestEnsureService(t *testing.T) {
 			validate: func(t *testing.T, cl client.Client, svc *corev1.Service) {
 				result := &corev1.Service{}
 				err := cl.Get(context.Background(), types.NamespacedName{
-					Name: svc.Name, Namespace: svc.Namespace}, result)
+					Name: svc.Name, Namespace: svc.Namespace,
+				}, result)
 				assert.NoError(t, err)
 				assert.Contains(t, result.GetAnnotations(), "old")
 				assert.Contains(t, result.GetAnnotations(), "new")
@@ -197,7 +202,8 @@ func TestEnsureService(t *testing.T) {
 			validate: func(t *testing.T, cl client.Client, svc *corev1.Service) {
 				result := &corev1.Service{}
 				err := cl.Get(context.Background(), types.NamespacedName{
-					Name: svc.Name, Namespace: svc.Namespace}, result)
+					Name: svc.Name, Namespace: svc.Namespace,
+				}, result)
 				assert.NoError(t, err)
 				assert.NotContains(t, result.GetAnnotations(), "old")
 				assert.NotContains(t, result.GetLabels(), "old")
@@ -248,7 +254,8 @@ func TestEnsureService(t *testing.T) {
 			validate: func(t *testing.T, cl client.Client, svc *corev1.Service) {
 				result := &corev1.Service{}
 				err := cl.Get(context.Background(), types.NamespacedName{
-					Name: svc.Name, Namespace: svc.Namespace}, result)
+					Name: svc.Name, Namespace: svc.Namespace,
+				}, result)
 				assert.NoError(t, err)
 				assert.Contains(t, result.GetAnnotations(), "ignore.annotation")
 				assert.Contains(t, result.GetAnnotations(), "keep")
@@ -300,7 +307,8 @@ func TestEnsureService(t *testing.T) {
 			validate: func(t *testing.T, cl client.Client, svc *corev1.Service) {
 				result := &corev1.Service{}
 				err := cl.Get(context.Background(), types.NamespacedName{
-					Name: svc.Name, Namespace: svc.Namespace}, result)
+					Name: svc.Name, Namespace: svc.Namespace,
+				}, result)
 				assert.NoError(t, err)
 				assert.Contains(t, result.GetLabels(), "ignore.label")
 				assert.Contains(t, result.GetLabels(), "keep")
@@ -330,6 +338,179 @@ func TestEnsureService(t *testing.T) {
 				if tt.validate != nil {
 					tt.validate(t, fakeClient, tt.svc)
 				}
+			}
+		})
+	}
+}
+
+func TestEqualMetadata(t *testing.T) {
+	tests := []struct {
+		name   string
+		input  []metav1.ObjectMeta
+		output bool
+	}{
+		{
+			name:   "empty input",
+			output: true,
+		},
+		{
+			name: "single input",
+			input: []metav1.ObjectMeta{
+				{Name: "meta"},
+			},
+			output: true,
+		},
+		{
+			name: "equal",
+			input: []metav1.ObjectMeta{
+				{
+					Name:                       "equal-meta1",
+					GenerateName:               "equal-",
+					Namespace:                  "test",
+					DeletionGracePeriodSeconds: new(int64),
+					Labels: map[string]string{
+						"test-label": "test-value",
+					},
+					Annotations: map[string]string{
+						"test-annotation": "test-value",
+					},
+					Finalizers: []string{
+						"test-fin-1",
+					},
+				},
+				{
+					Name:                       "equal-meta1",
+					GenerateName:               "equal-",
+					Namespace:                  "test",
+					DeletionGracePeriodSeconds: new(int64),
+					Labels: map[string]string{
+						"test-label": "test-value",
+					},
+					Annotations: map[string]string{
+						"test-annotation":                        "test-value",
+						naming.AnnotationLastConfigHash.String(): "true",
+					},
+					Finalizers: []string{
+						"test-fin-1",
+					},
+
+					SelfLink:        "selflink1",
+					UID:             "uid1",
+					ResourceVersion: "resourceVer1",
+					Generation:      1,
+					CreationTimestamp: metav1.Time{
+						Time: time.Now().Add(-time.Second),
+					},
+					DeletionTimestamp: &metav1.Time{
+						Time: time.Now().Add(time.Second),
+					},
+					OwnerReferences: []metav1.OwnerReference{
+						{
+							APIVersion: "apiVersion1",
+						},
+					},
+					ManagedFields: []metav1.ManagedFieldsEntry{
+						{
+							APIVersion: "apiVersion1",
+						},
+					},
+				},
+				{
+					Name:                       "equal-meta1",
+					GenerateName:               "equal-",
+					Namespace:                  "test",
+					DeletionGracePeriodSeconds: new(int64),
+					Labels: map[string]string{
+						"test-label": "test-value",
+					},
+					Annotations: map[string]string{
+						"test-annotation":                        "test-value",
+						naming.AnnotationLastConfigHash.String(): "false",
+					},
+					Finalizers: []string{
+						"test-fin-1",
+					},
+
+					SelfLink:        "selflink2",
+					UID:             "uid2",
+					ResourceVersion: "resourceVer2",
+					Generation:      2,
+					CreationTimestamp: metav1.Time{
+						Time: time.Now().Add(time.Second),
+					},
+					DeletionTimestamp: &metav1.Time{
+						Time: time.Now().Add(-time.Second),
+					},
+					OwnerReferences: []metav1.OwnerReference{
+						{
+							APIVersion: "apiVersion2",
+						},
+					},
+					ManagedFields: []metav1.ManagedFieldsEntry{
+						{
+							APIVersion: "apiVersion2",
+						},
+					},
+				},
+			},
+			output: true,
+		},
+		{
+			name: "different names",
+			input: []metav1.ObjectMeta{
+				{Name: "equal-meta1"},
+				{Name: "equal-meta2"},
+			},
+		},
+		{
+			name: "different generateName",
+			input: []metav1.ObjectMeta{
+				{GenerateName: "equal-"},
+				{GenerateName: "equal2-"},
+			},
+		},
+		{
+			name: "different namespaces",
+			input: []metav1.ObjectMeta{
+				{Namespace: "ns"},
+				{Namespace: "ns2"},
+			},
+		},
+		{
+			name: "different deletionGracePeriodSeconds",
+			input: []metav1.ObjectMeta{
+				{},
+				{DeletionGracePeriodSeconds: new(int64)},
+			},
+		},
+		{
+			name: "different labels",
+			input: []metav1.ObjectMeta{
+				{Labels: map[string]string{"test-label": "test-value"}},
+				{Labels: map[string]string{"test-label": "test-value2"}},
+			},
+		},
+		{
+			name: "different annotations",
+			input: []metav1.ObjectMeta{
+				{Annotations: map[string]string{"test-annotation": "test-value"}},
+				{Annotations: map[string]string{"test-annotation": "test-value2"}},
+			},
+		},
+		{
+			name: "different finalizers",
+			input: []metav1.ObjectMeta{
+				{Finalizers: []string{"test-fin-1"}},
+				{Finalizers: []string{"test-fin-2"}},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(t.Name(), func(t *testing.T) {
+			result := EqualMetadata(tt.input...)
+			if result != tt.output {
+				t.Errorf("Expected %v, got %v", tt.output, result)
 			}
 		})
 	}
