bbbdb695ad6b64049710e9632e360145959c5621
Eleonora Zinchenko <eleonora.zinchenko@percona.com>
2025-09-15 23:17:42 +0300

    Fix unbound variable

--- e2e-tests/vars.sh
+++ e2e-tests/vars.sh
@@ -11,8 +11,8 @@ export GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 export VERSION=${VERSION:-$(echo "${GIT_BRANCH}" | sed -e 's^/^-^g; s^[.]^-^g;' | tr '[:upper:]' '[:lower:]')}
 
 export IMAGE=${IMAGE:-"perconalab/percona-server-mysql-operator:${VERSION}"}
-if [[ -z $MYSQL_VERSION && -n $IMAGE_MYSQL ]]; then
-	export MYSQL_VERSION=$(echo $IMAGE_MYSQL | sed -E 's/^[^0-9]*([0-9]+\.[0-9]+).*/\1/')
+if [[ -z ${MYSQL_VERSION-} && -n ${IMAGE_MYSQL-} ]]; then
+	export MYSQL_VERSION=$(echo "$IMAGE_MYSQL" | sed -E 's/.*://; s/^[^0-9]*([0-9]+\.[0-9]+).*/\1/')
 else
 	export MYSQL_VERSION=${MYSQL_VERSION:-"8.4"}
 fi
