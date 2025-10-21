e80230bc6334074348bc500dd7b858e0542c78e1
Viacheslav Sarzhan <slava.sarzhan@percona.com>
2025-10-21 23:40:39 +0300

    improve heartbeat entrypoint

--- build/heartbeat-entrypoint.sh
+++ build/heartbeat-entrypoint.sh
@@ -19,15 +19,25 @@ MYSQL_PASSWORD=$(cat /etc/mysql/mysql-users-secret/monitor || :)
 TIMEOUT="${CLONE_TIMEOUT_SECONDS:-3600}"
 MYSQL_CMDLINE="/usr/bin/timeout 10 /usr/bin/mysql -nNE -u$MYSQL_USER"
 
-for i in {1..5}; do
+# Check clone status every 5 seconds up to TIMEOUT
+CHECK_INTERVAL=5
+ELAPSED=0
+
+while [ $ELAPSED -lt $TIMEOUT ]; do
 	CLONE_STATUS=$(MYSQL_PWD=${MYSQL_PASSWORD} $MYSQL_CMDLINE -P$MYSQL_ADMIN_PORT -e 'SELECT STATE FROM performance_schema.clone_status;' | sed -n -e '2p' | tr -d '\n')
 	if [[ $CLONE_STATUS == "Completed" || -z $CLONE_IN_PROGRESS ]]; then
-		echo '[INFO] Started pt-heartbeat'
+		echo '[INFO] Clone completed, starting pt-heartbeat'
 		break
-	else
-		echo "[INFO] Waiting for MySQL initialization $((TIMEOUT * i))"
-		sleep "$((TIMEOUT * i))"
 	fi
+
+	echo "[INFO] Waiting for MySQL initialization (${ELAPSED}s/${TIMEOUT}s)"
+
+	# Sleep in 1-second intervals to allow signal handling
+	for ((j=0; j<CHECK_INTERVAL; j++)); do
+		sleep 1
+	done
+
+	ELAPSED=$((ELAPSED + CHECK_INTERVAL))
 done
 
 # If password contains commas they must be escaped with a backslash: “exam,ple” according https://docs.percona.com/percona-toolkit/pt-heartbeat.html
