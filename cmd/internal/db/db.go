9ffecce69e5fde5313885f5aa9f3d132bbff5335
Ege Güneş <ege.gunes@percona.com>
2025-10-31 13:13:42 +0300

    fix split-brain detecion in liveness probe

--- cmd/internal/db/db.go
+++ cmd/internal/db/db.go
@@ -341,30 +341,39 @@ func (d *DB) GetMemberState(ctx context.Context, host string) (db.MemberState, e
 }
 
 func (d *DB) CheckIfInPrimaryPartition(ctx context.Context) (bool, error) {
-	var hasQuorum bool
+	var in bool
 
 	err := d.db.QueryRowContext(ctx, `
 	SELECT
-		(
-			SELECT COUNT(*)
-			FROM performance_schema.replication_group_members
-			WHERE MEMBER_STATE IN ('ONLINE', 'RECOVERING')
-		) * 2 > (
-			SELECT COUNT(*)
-			FROM performance_schema.replication_group_members
-			WHERE MEMBER_STATE IN ('ONLINE', 'RECOVERING')
+		MEMBER_STATE = 'ONLINE'
+		AND (
+			(
+				SELECT
+					COUNT(*)
+				FROM
+					performance_schema.replication_group_members
+				WHERE
+					MEMBER_STATE NOT IN ('ONLINE', 'RECOVERING')
+			) >= (
+				(
+					SELECT
+						COUNT(*)
+					FROM
+						performance_schema.replication_group_members
+				) / 2
+			) = 0
 		)
-		AND MEMBER_STATE IN ('ONLINE', 'RECOVERING')
 	FROM
 		performance_schema.replication_group_members
+		JOIN performance_schema.replication_group_member_stats USING(member_id)
 	WHERE
-		member_id = @@global.server_uuid
-	`).Scan(&hasQuorum)
+		member_id = @@global.server_uuid;
+	`).Scan(&in)
 	if err != nil {
 		return false, err
 	}
 
-	return hasQuorum, nil
+	return in, nil
 }
 
 func (d *DB) EnableSuperReadonly(ctx context.Context) error {
