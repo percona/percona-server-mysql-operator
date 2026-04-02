235ef31762d81615957a2cf09f11ba716ebd038e
Mayank Shah <mayank.shah@percona.com>
2026-04-02 16:03:26 +0530

    copilot
    Signed-off-by: Mayank Shah <mayank.shah@percona.com>

--- pkg/db/replication.go
+++ pkg/db/replication.go
@@ -125,8 +125,7 @@ func (m *ReplicationDBManager) ReplicationStatus(ctx context.Context) (Replicati
 	if rows[0].IoState == "ON" && rows[0].SqlState == "ON" {
 		return ReplicationStatusActive, rows[0].Host, nil
 	}
-
-	return ReplicationStatusStopped, "", err
+	return ReplicationStatusStopped, "", nil
 }
 
 func (m *ReplicationDBManager) GetGroupReplicationPrimary(ctx context.Context) (string, error) {
