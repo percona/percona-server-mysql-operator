b7997fcc0716cc81853f8db4eb73e51025cf626e
Ege Güneş <ege.gunes@percona.com>
2026-04-13 22:45:28 +0300

    fix

--- pkg/controller/psrestore/controller.go
+++ pkg/controller/psrestore/controller.go
@@ -181,9 +181,9 @@ func (r *PerconaServerMySQLRestoreReconciler) Reconcile(ctx context.Context, req
 	defer r.sm.Delete(cr.Spec.ClusterName)
 
 	if cr.Spec.PITR != nil {
-		if !cluster.Spec.Backup.PiTR.Enabled || cluster.Spec.Backup.PiTR.BinlogServer == nil {
+		if cluster.Spec.Backup.PiTR.BinlogServer == nil {
 			status.State = apiv1.RestoreError
-			status.StateDesc = "PITR is not enabled for the cluster"
+			status.StateDesc = "Binlog server is not enabled for the cluster"
 			return ctrl.Result{}, nil
 		}
 
