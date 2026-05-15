1922904579df33be6c5c6f9e9b9edd79aacd2fd8
Andrii Dema <a.dema@jazzserve.com>
2026-05-15 13:18:26 +0300

    change test

--- pkg/controller/psrestore/controller.go
+++ pkg/controller/psrestore/controller.go
@@ -196,7 +196,7 @@ func (r *PerconaServerMySQLRestoreReconciler) Reconcile(ctx context.Context, req
 	defer r.sm.Delete(cr.Spec.ClusterName)
 
 	if cr.Spec.PITR != nil {
-		if !cluster.Spec.Backup.PiTR.Enabled || (cluster.Spec.Backup.PiTR.BinlogServer == nil && restoreBinlogServer(cr) == nil) {
+		if cluster.Spec.Backup.PiTR.BinlogServer == nil && restoreBinlogServer(cr) == nil {
 			status.State = apiv1.RestoreError
 			status.StateDesc = "Binlog server is not enabled for the cluster"
 			return ctrl.Result{}, nil
@@ -354,7 +354,7 @@ func (r *PerconaServerMySQLRestoreReconciler) reconcileBackupSourceBinlogServer(
 	cr *apiv1.PerconaServerMySQLRestore,
 	cluster *apiv1.PerconaServerMySQL,
 ) error {
-	if cr.Spec.PITR == nil || !cluster.Spec.Backup.PiTR.Enabled || restoreBinlogServer(cr) == nil {
+	if cr.Spec.PITR == nil || restoreBinlogServer(cr) == nil {
 		return nil
 	}
 
