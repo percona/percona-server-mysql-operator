cc0e08f9cbc4c068ebeb9a9b3a2ac25719d6edba
Oksana Grishchenko <oksana.grishchenko@percona.com>
2026-06-12 14:22:26 +0500

    only preserve imageID when haproxy enabled
    Signed-off-by: Oksana Grishchenko <oksana.grishchenko@percona.com>

--- pkg/controller/ps/status.go
+++ pkg/controller/ps/status.go
@@ -138,8 +138,8 @@ func (r *PerconaServerMySQLReconciler) reconcileCRStatus(ctx context.Context, cr
 			if err != nil {
 				return errors.Wrap(err, "get HAProxy status")
 			}
+			haproxyStatus.ImageID = status.HAProxy.ImageID
 		}
-		haproxyStatus.ImageID = status.HAProxy.ImageID
 		status.HAProxy = haproxyStatus
 
 		binlogServerStatus := apiv1.StatefulAppStatus{}
