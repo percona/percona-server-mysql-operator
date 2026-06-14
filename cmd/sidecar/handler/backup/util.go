cb9ac8b1e5f1a78c7edf0d8542ddf9a8413e4205
George Kechagias <george.kechagias@percona.com>
2026-06-14 17:15:37 +0300

    cr: use validation.IsDNS1123Subdomain

--- cmd/sidecar/handler/backup/util.go
+++ cmd/sidecar/handler/backup/util.go
@@ -8,13 +8,10 @@ import (
 	"strings"
 
 	"github.com/pkg/errors"
+	"k8s.io/apimachinery/pkg/util/validation"
 
 	xb "github.com/percona/percona-server-mysql-operator/pkg/xtrabackup"
 )
-
-// backupNameRE matches RFC 1123 subdomain names, the format Kubernetes
-// resource names follow.
-var backupNameRE = regexp.MustCompile(`^[a-z0-9]([-a-z0-9.]*[a-z0-9])?$`)
 
 // ErrInvalidBackupName is returned by ValidateBackupName for any input
 // that is not a valid RFC 1123 subdomain.
@@ -24,14 +21,8 @@ var ErrInvalidBackupName = errors.New("invalid backup name")
 // backup log directory when joined into a file path. backupName is taken
 // from the HTTP request URL, so untrusted input reaches filepath.Join.
 func ValidateBackupName(backupName string) error {
-	if backupName == "" {
-		return errors.Wrap(ErrInvalidBackupName, "empty")
-	}
-	if len(backupName) > 253 {
-		return errors.Wrapf(ErrInvalidBackupName, "exceeds 253 characters: %d", len(backupName))
-	}
-	if !backupNameRE.MatchString(backupName) {
-		return errors.Wrapf(ErrInvalidBackupName, "%q is not a valid RFC 1123 subdomain", backupName)
+	if errs := validation.IsDNS1123Subdomain(backupName); len(errs) > 0 {
+		return errors.Wrapf(ErrInvalidBackupName, "%q: %s", backupName, strings.Join(errs, "; "))
 	}
 	return nil
 }
