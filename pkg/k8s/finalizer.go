e05b737facbce298f0e8ab75b5f0569f6669a3bf
Mayank Shah <mayank.shah@percona.com>
2026-07-28 13:41:05 +0530

    remove retries for patch
    Signed-off-by: Mayank Shah <mayank.shah@percona.com>

--- pkg/k8s/finalizer.go
+++ pkg/k8s/finalizer.go
@@ -3,34 +3,11 @@ package k8s
 import (
 	"context"
 
-	k8sretry "k8s.io/client-go/util/retry"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 )
 
-func RemoveFinalizers(
-	ctx context.Context,
-	c client.Client,
-	obj client.Object,
-	finalizers ...string,
-) error {
-	return k8sretry.RetryOnConflict(k8sretry.DefaultBackoff, func() error {
-		return removeFinalizers(ctx, c, obj, finalizers...)
-	})
-}
-
 func SetFinalizers(
-	ctx context.Context,
-	c client.Client,
-	obj client.Object,
-	finalizers ...string,
-) error {
-	return k8sretry.RetryOnConflict(k8sretry.DefaultBackoff, func() error {
-		return setFinalizers(ctx, c, obj, finalizers...)
-	})
-}
-
-func setFinalizers(
 	ctx context.Context,
 	c client.Client,
 	obj client.Object,
@@ -57,7 +34,7 @@ func setFinalizers(
 	return c.Patch(ctx, o, client.MergeFrom(orig))
 }
 
-func removeFinalizers(
+func RemoveFinalizers(
 	ctx context.Context,
 	c client.Client,
 	obj client.Object,
