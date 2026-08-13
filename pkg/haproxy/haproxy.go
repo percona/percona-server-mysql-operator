04a77b3d0870b29489133226635cb4f2dfd11742
Ege Güneş <ege.gunes@percona.com>
2026-08-13 13:24:43 +0300

    K8SPS-856: Don't reload haproxy on DNS changes

--- pkg/haproxy/haproxy.go
+++ pkg/haproxy/haproxy.go
@@ -351,6 +351,19 @@ func haproxyContainer(cr *apiv1.PerconaServerMySQL) corev1.Container {
 		env = append(env, probsEnvs...)
 	}
 
+	if cr.CompareVersion("1.3.0") >= 0 {
+		env = append(env, []corev1.EnvVar{
+			{
+				Name:  "CLUSTER_NAME",
+				Value: cr.Name,
+			},
+			{
+				Name:  "HA_MAX_SLOTS",
+				Value: strconv.Itoa(int(max(9, cr.Spec.MySQL.Size))),
+			},
+		}...)
+	}
+
 	volumeMounts := []corev1.VolumeMount{
 		{
 			Name:      "bin",
@@ -437,6 +450,19 @@ func mysqlMonitContainer(cr *apiv1.PerconaServerMySQL) corev1.Container {
 			},
 		}
 		env = append(env, cluserTypeEnv...)
+	}
+
+	if cr.CompareVersion("1.3.0") >= 0 {
+		env = append(env, []corev1.EnvVar{
+			{
+				Name:  "CLUSTER_NAME",
+				Value: cr.Name,
+			},
+			{
+				Name:  "HA_MAX_SLOTS",
+				Value: strconv.Itoa(int(max(9, cr.Spec.MySQL.Size))),
+			},
+		}...)
 	}
 
 	resources := spec.Resources
