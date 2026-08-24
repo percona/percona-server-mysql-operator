23fba63328007d923bcf8dbf75cf5b7307903a9c
Oksana Grishchenko <oksana.grishchenko@percona.com>
2026-08-24 20:19:54 +0500

    no PXB ticket
    Signed-off-by: Oksana Grishchenko <oksana.grishchenko@percona.com>

--- cmd/sidecar/handler/handler.go
+++ cmd/sidecar/handler/handler.go
@@ -95,7 +95,7 @@ func GetBackupInfoFunc(w http.ResponseWriter, req *http.Request) {
 		log.Info("failed to get backup info from xtrabackup_info, skipping backup size", "error", err)
 	} else if backupInfo.BackupSize == 0 {
 		// For compressed backups, xtrabackup_info is stored as xtrabackup_info.zst
-		// Need to decompress it until [!!!insert link to PXB ticket!!!] is fixed.
+		// Need to decompress it first.
 		compressedInfo, err := fetchXbcloudFileDecompressed(req.Context(), log, &backupConf, "xtrabackup_info.zst")
 		if err != nil {
 			log.Info("failed to get compressed xtrabackup_info.zst, skipping backup size", "error", err)
