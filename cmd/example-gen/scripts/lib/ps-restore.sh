361df0e18385d42df1e5a8496e1dd378d8503e40
Oksana Grishchenko <oksana.grishchenko@percona.com>
2026-08-24 15:25:55 +0500

    manifests
    Signed-off-by: Oksana Grishchenko <oksana.grishchenko@percona.com>

--- cmd/example-gen/scripts/lib/ps-restore.sh
+++ cmd/example-gen/scripts/lib/ps-restore.sh
@@ -57,6 +57,7 @@ remove_fields() {
 		| yq "del(.spec.backupSource.completed)" \
 		| yq "del(.spec.backupSource.image)" \
 		| yq "del(.spec.backupSource.size)" \
+		| yq "del(.spec.backupSource.uncompressedSize)" \
 		| yq "del(.spec.backupSource.state)" \
 		| yq "del(.spec.backupSource.stateDescription)" \
 		| yq "del(.spec.backupSource.storage.azure)" \
