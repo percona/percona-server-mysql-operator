491f5baf11297c3d8f8a3de8fce96533a309cef3
Viacheslav Sarzhan <slava.sarzhan@percona.com>
2026-08-30 13:29:27 +0300

    fix test

--- e2e-tests/vars.sh
+++ e2e-tests/vars.sh
@@ -55,8 +55,6 @@ set_image() {
 set_image IMAGE "perconalab/percona-server-mysql-operator:${VERSION}"
 set_image IMAGE_MYSQL "perconalab/percona-server-mysql-operator:main-psmysql${MYSQL_VERSION}"
 set_image IMAGE_BACKUP "perconalab/percona-server-mysql-operator:main-backup${MYSQL_VERSION}"
-set_image IMAGE_MYSQL97 "percona/percona-server:9.7"
-set_image IMAGE_BACKUP97 "percona/percona-xtrabackup:9.7"
 set_image IMAGE_ORCHESTRATOR "perconalab/percona-server-mysql-operator:main-orchestrator"
 set_image IMAGE_ROUTER "perconalab/percona-server-mysql-operator:main-router${MYSQL_VERSION}"
 set_image IMAGE_TOOLKIT "perconalab/percona-server-mysql-operator:main-toolkit"
