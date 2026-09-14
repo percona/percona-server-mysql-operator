666b3809268fdc6305763259e561d966d1910425
George Kechagias <george.kechagias@percona.com>
2026-09-14 14:17:53 +0300

    cr: adjust storage size

--- cmd/example-gen/pkg/defaults/preset.go
+++ cmd/example-gen/pkg/defaults/preset.go
@@ -106,7 +106,7 @@ func FromPresets(cr any) error {
 				},
 				Resources: corev1.VolumeResourceRequirements{
 					Requests: corev1.ResourceList{
-						corev1.ResourceStorage: resource.MustParse("28Gi"),
+						corev1.ResourceStorage: resource.MustParse("30Gi"),
 					},
 				},
 			},
